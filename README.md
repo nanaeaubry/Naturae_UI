b695d1337d85add1dc34d64d32f4033963e675ea
nanaeaubry <nanaeaubry@gmail.com>
2019-01-31 12:14:46 -0800

    Updated ReadMe with information

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Naturae_UI
+# Naturae
 
-#Install Android Studio
+Senior Group Project for California State Univerity Long Beach
+
+Class: CECS 491 - Spring 2019
+
+## Description
+
+A crowd-sourcing application for nature lovers, observers, and researchers.
+
+## Know limitations
+
+This project has been designed to run on Android Devices through the use of
+Java in Android Studio. 
+
+We have not tested on other platforms.
+
+### Contributors
+- Brian Ashley
+- Nanae Aubry
+- Marcus Herndon
+- Visal Hok
+- Steven Lim
--- app/src/main/java/com/example/naturae_ui/MainActivity.java
+++ app/src/main/java/com/example/naturae_ui/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
 		@Override
 		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 			switch (item.getItemId()) {
-				case R.id.navigation_home:
-					mTextMessage.setText(R.string.title_home);
+				case R.id.navigation_map:
+					mTextMessage.setText(R.string.title_map);
 					return true;
-				case R.id.navigation_dashboard:
-					mTextMessage.setText(R.string.title_dashboard);
+				case R.id.navigation_post:
+					mTextMessage.setText(R.string.title_post);
 					return true;
-				case R.id.navigation_notifications:
-					mTextMessage.setText(R.string.title_notifications);
+				case R.id.navigation_profile:
+					mTextMessage.setText(R.string.title_profile);
 					return true;
 			}
 			return false;
