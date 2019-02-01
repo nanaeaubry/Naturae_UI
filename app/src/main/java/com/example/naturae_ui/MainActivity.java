485e53793bfa15c0ba1464bccbc58bd1c184e1d5
nanaeaubry <nanaeaubry@gmail.com>
2019-02-01 14:07:33 -0800

    Add Map components

--- app/src/main/java/com/example/naturae_ui/MainActivity.java
+++ app/src/main/java/com/example/naturae_ui/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.naturae_ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private TextView mTextMessage;
+
+	private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+			= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+		@Override
+		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+			switch (item.getItemId()) {
+				case R.id.navigation_map:
+					return true;
+				case R.id.navigation_post:
+					mTextMessage.setText(R.string.title_post);
+					return true;
+				case R.id.navigation_profile:
+					mTextMessage.setText(R.string.title_profile);
+					return true;
+			}
+			return false;
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mTextMessage = (TextView) findViewById(R.id.message);
+		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+		//Show Map on Main Activity
+		Intent nextScreen = new Intent(getApplicationContext(), MapsActivity.class);
+		startActivity(nextScreen);
+	}
 }
--- app/src/main/java/com/example/naturae_ui/MapsActivity.java
+++ app/src/main/java/com/example/naturae_ui/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.naturae_ui;
+
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 	private GoogleMap mMap;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,30 +38,5 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.map_options, menu);
-		return true;
-	}
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Change the map type based on the user's selection.
-		switch (item.getItemId()) {
-			case R.id.normal_map:
-				mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-				return true;
-			case R.id.hybrid_map:
-				mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-				return true;
-			case R.id.satellite_map:
-				mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-				return true;
-			case R.id.terrain_map:
-				mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
+
 }
