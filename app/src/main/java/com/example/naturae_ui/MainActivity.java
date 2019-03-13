9b9e9398cf172c77814f8ee14b42fd776aedaef7
nanaeaubry <nanaeaubry@gmail.com>
2019-03-12 18:07:40 -0700

    Removed Map Activity. Add Post layout

--- app/src/main/java/com/example/naturae_ui/MainActivity.java
+++ app/src/main/java/com/example/naturae_ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.naturae_ui;
 
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,7 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 	private TextView mTextMessage;
 
@@ -33,18 +40,26 @@ public class MainActivity extends AppCompatActivity {
 	};
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
+
+		// Retrieve the content view that renders the map.
 		setContentView(R.layout.activity_main);
 
-		mTextMessage = (TextView) findViewById(R.id.message);
-		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
-
-//		//Show Map on Main Activity
-//		Intent nextScreen = new Intent(getApplicationContext(), MapsActivity.class);
-//		startActivity(nextScreen);
+		// Get the SupportMapFragment and request notification
+		// when the map is ready to be used.
+		SupportMapFragment mapFragment = (SupportMapFragment)
+				getSupportFragmentManager()
+						.findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
+	}
+	@Override
+	public void onMapReady(GoogleMap googleMap)
+	{
+		// Add a marker in Sydney, Australia,
+		// and move the map's camera to the same location.
+		LatLng myPos = new LatLng(Location.getLatitude(), Location.getLongitude());
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
 	}
 }
