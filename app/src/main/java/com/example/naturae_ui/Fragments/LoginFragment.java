26022142f26fbcfa657983bf9f2048d1b0c0330b
HappyLyfe123 <visalhok123@gmail.com>
2019-04-02 21:33:12 -0700

    Create GRPC test call

--- app/src/main/java/com/example/naturae_ui/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/naturae_ui/Fragments/LoginFragment.java
@@ -1,22 +1,38 @@
 package com.example.naturae_ui.Fragments;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.naturae_ui.Containers.StartUpContainer;
 import com.example.naturae_ui.R;
 
-import org.w3c.dom.Text;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.ref.WeakReference;
+import java.util.Objects;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.okhttp.OkHttpChannelBuilder;
+
+import com.examples.naturaeproto.Naturae;
+import com.examples.naturaeproto.ServerRequestsGrpc;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 public class LoginFragment extends Fragment implements View.OnClickListener{
 
@@ -26,7 +42,6 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     private TextInputEditText passwordEditText;
     private TextView wrongCredentialTextView;
     private ImageView appNameImage;
-
 
     private View view;
 
@@ -51,13 +66,13 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_login, container, false);
         //Assign all of the variable in the fragment
-        emailEditText = (TextInputEditText) view.findViewById(R.id.email_edit_text);
-        passwordEditText = (TextInputEditText) view.findViewById(R.id.password_edit_text);
-        wrongCredentialTextView = (TextView) view.findViewById(R.id.wrong_credential_text_view);
-        Button forgetPasswordTextView = (Button) view.findViewById(R.id.forget_password_text_view);
-        Button loginButton = (Button) view.findViewById(R.id.login_button);
-        Button createAccountButton = (Button) view.findViewById(R.id.create_account_button);
-        appNameImage = (ImageView) view.findViewById(R.id.app_name_image_view);
+        emailEditText = view.findViewById(R.id.email_edit_text);
+        passwordEditText =  view.findViewById(R.id.password_edit_text);
+        wrongCredentialTextView = view.findViewById(R.id.wrong_credential_text_view);
+        Button forgetPasswordTextView =  view.findViewById(R.id.forget_password_text_view);
+        Button loginButton = view.findViewById(R.id.login_button);
+        Button createAccountButton = view.findViewById(R.id.create_account_button);
+        appNameImage = view.findViewById(R.id.app_name_image_view);
 
         //Set up listener
         loginButton.setOnClickListener(this);
@@ -106,9 +121,10 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
         switch (v.getId()){
             //Login button selected
             case R.id.login_button:
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
+                String email = Objects.requireNonNull(emailEditText.getText()).toString();
+                String password = Objects.requireNonNull(passwordEditText.getText()).toString();
                 mListener.hideKeyboard();
+                login();
                 break;
             //Create account selected
             case R.id.create_account_button:
@@ -127,10 +143,41 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     /**
      * Preform login process
      */
-    private void login(){
-        Thread loginThread = new Thread(()->{
+    public void login(){
+        new GrpcTask(this).execute();
+    }
 
-        });
+    private static class GrpcTask extends AsyncTask<Void, Void, String>{
+
+        private final WeakReference<Fragment> fragmentReference;
+        private ManagedChannel channel;
+        public GrpcTask(LoginFragment loginFragment) {
+            this.fragmentReference = new WeakReference<>(loginFragment);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try{
+                channel = OkHttpChannelBuilder.forAddress("naturae.host", 443).useTransportSecurity().build();
+                ServerRequestsGrpc.ServerRequestsBlockingStub stub = ServerRequestsGrpc.newBlockingStub(channel);
+                stub.sayHello(Naturae.HelloRequest.newBuilder().setName("Visal").build());
+
+            }catch (Exception e){
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                pw.flush();
+                System.out.printf("Failed... : %n%s", sw);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
     }
 
 }
