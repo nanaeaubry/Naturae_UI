266531fc36e04ea0076b8e7ffee0c0c6a5497eca
nanaeaubry <nanaeaubry@gmail.com>
2019-04-08 21:17:40 -0700

    Added camera permission and ability to extract information from image

--- app/src/main/java/com/example/naturae_ui/Fragments/PostFragment.java
+++ app/src/main/java/com/example/naturae_ui/Fragments/PostFragment.java
@@ -5,14 +5,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,23 +24,26 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
+
 
 import com.example.naturae_ui.Models.Post;
 import com.example.naturae_ui.R;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-import io.grpc.Metadata;
 
 import static android.view.View.GONE;
 
 public class PostFragment extends Fragment {
 	// PICK_PHOTO_CODE is a constant integer
-	public final static int PICK_PHOTO_CODE = 1046;
+	public final static int PICK_PHOTO = 1046;
+	public final static int REQUEST_IMAGE_CAPTURE = 1047;
+	public final String APP_TAG = "NaturaePhotos";
+	public String photoFileName = "photo.jpg";
+	File photoFile;
+	Uri photoFileUri;
 
 	View mView;
 	ImageButton openCamera;
@@ -57,11 +64,26 @@ public class PostFragment extends Fragment {
 		mView = inflater.inflate(R.layout.fragment_post, container, false);
 		super.onCreate(savedInstanceState);
 
+		imagePreview = mView.findViewById(R.id.image_preview);
+
+		// Create a File reference for photo capture
+		photoFile = getPhotoFile(photoFileName);
+
+		// wrap File object into a content provider
+		photoFileUri = FileProvider.getUriForFile(getContext(), "com.example.naturae_ui", photoFile);
+
 		//Button to open camera on user phone
 		openCamera = mView.findViewById(R.id.open_camera);
 		openCamera.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFileUri);
+
+				if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+					startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+				}
 			}
 		});
 
@@ -77,7 +99,7 @@ public class PostFragment extends Fragment {
 				// So as long as the result is not null, it's safe to use the intent.
 				if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
 					// Bring up gallery to select a photo
-					startActivityForResult(intent, PICK_PHOTO_CODE);
+					startActivityForResult(intent, PICK_PHOTO);
 				}
 			}
 		});
@@ -136,24 +158,60 @@ public class PostFragment extends Fragment {
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (data != null) {
-			// Do something with the photo based on Uri
-			try {
+		if (data == null) {
+			return;
+		}
 
-				Uri photoUri = data.getData();
-				selectedImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+		switch (requestCode) {
+			case PICK_PHOTO:
+				// Do something with the photo based on Uri
+				try {
 
-				// Load the selected image into a preview
-				imagePreview = mView.findViewById(R.id.image_preview);
+					Uri photoUri = data.getData();
+					selectedImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+
+					// Load the selected image into a preview
+					imagePreview.setVisibility(View.VISIBLE);
+					imagePreview.setImageBitmap(selectedImage);
+
+					readExif(photoUri);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				break;
+
+			case REQUEST_IMAGE_CAPTURE:
+				// by this point we have the camera photo on disk
+				selectedImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+				// RESIZE BITMAP, see section below
+				// Load the taken image into a preview
+				ImageView imagePreview = mView.findViewById(R.id.image_preview);
 				imagePreview.setVisibility(View.VISIBLE);
 				imagePreview.setImageBitmap(selectedImage);
 
-				readExif(photoUri);
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+				readExif(photoFileUri);
+				break;
 		}
+	}
+
+	// Returns the File for a photo stored on disk given the fileName
+	public File getPhotoFile(String fileName) {
+		// Get safe storage directory for photos
+		// Use `getExternalFilesDir` on Context to access package-specific directories.
+		// This way, we don't need to request external read/write runtime permissions.
+		File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+			Log.d(APP_TAG, "failed to create directory");
+		}
+
+		// Return the file target for the photo based on filename
+		File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+		return file;
 	}
 
 	void readExif(Uri uri) {
