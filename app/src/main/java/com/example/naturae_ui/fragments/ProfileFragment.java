fb94a1f2b68e744a28b4a8f506c7c47aedd82ce3
Brian Ashley <bsashley05@gmail.com>
2019-05-01 17:08:20 -0700

    added fragments, xml, and proto back into project

--- app/src/main/java/com/example/naturae_ui/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/naturae_ui/fragments/ProfileFragment.java
@@ -1,30 +1,52 @@
 package com.example.naturae_ui.fragments;
 
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.example.naturae_ui.R;
-import com.example.naturae_ui.containers.StartUpActivityContainer;
+import com.example.naturae_ui.containers.MainActivityContainer;
+import com.example.naturae_ui.util.Constants;
+import com.example.naturae_ui.util.Helper;
 import com.example.naturae_ui.util.UserUtilities;
+import com.examples.naturaeproto.Naturae;
+import com.examples.naturaeproto.ServerRequestsGrpc;
 
-import java.util.Objects;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
 
-public class ProfileFragment extends Fragment implements LoginFragment.OnFragmentInteractionListener {
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import static com.example.naturae_ui.fragments.PostFragment.REQUEST_IMAGE_CAPTURE;
+
+public class ProfileFragment extends Fragment {
     public final static int PICK_PHOTO = 1046;
     View mView;
     EditText firstName;
@@ -33,6 +55,14 @@ public class ProfileFragment extends Fragment implements LoginFragment.OnFragmen
     Button bLogout;
     Button bChangePass;
     ImageButton ibProfileImage;
+    OnFragmentInteractionListener mListener;
+    EditText currentPassword;
+    EditText newPassword;
+    EditText confirmPassword;
+    Bitmap selectedImage = null;
+    ImageView imagePreview;
+    File photoFile;
+    Uri photoFileUri;
 
 
     @Nullable
@@ -48,11 +78,7 @@ public class ProfileFragment extends Fragment implements LoginFragment.OnFragmen
 
         bChangePass = mView.findViewById(R.id.btChangePass);
         bChangePass.setOnClickListener(v -> {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            ChangePasswordFragment changePasswordFragment = new ChangePasswordFragment();
-            fragmentTransaction.replace(R.id.fragment_container, changePasswordFragment);
-            fragmentTransaction.commit();
+            mListener.beginFragment(MainActivityContainer.AuthFragmentType.CHANGE_PASSWORD, true, true);
         });
 
 
@@ -68,39 +94,182 @@ public class ProfileFragment extends Fragment implements LoginFragment.OnFragmen
                 startActivityForResult(intent, PICK_PHOTO);
             }
         });
-        /*bLogout =  mView.findViewById(R.id.btLogout);
+        bLogout = mView.findViewById(R.id.btLogout);
         bLogout.setOnClickListener(v -> {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            LoginFragment loginFragment = new LoginFragment();
-            fragmentTransaction.replace(R.id.fragment_container, loginFragment);
-            fragmentTransaction.commit();
-        });*/
+            UserUtilities.removeCachedUserInfo(getActivity());
+            mListener.logout();
+        });
         return mView;
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof ProfileFragment.OnFragmentInteractionListener) {
+            mListener = (ProfileFragment.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    public interface OnFragmentInteractionListener {
+        void beginFragment(MainActivityContainer.AuthFragmentType fragmentType, boolean setTransition,
+                           boolean addToBackStack);
+
+        void logout();
+    }
+
+    /**
+     * Get image data and process accordingly based on whether photo is taken with camera
+     * or uploaded from user gallery
+     *
+     * @param requestCode specifies how picture was gotten
+     * @param resultCode  what will happen with image
+     * @param data        data about the image
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (data == null) {
             return;
         }
-        Uri photoUri = data.getData();
-        ibProfileImage.setImageURI(photoUri);
+
+        switch (requestCode) {
+            case PICK_PHOTO:
+                // Do something with the photo based on Uri
+                try {
+
+                    Uri photoUri = data.getData();
+                    selectedImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+
+                    // Load the selected image into a preview
+                    imagePreview.setVisibility(View.VISIBLE);
+                    imagePreview.setImageBitmap(selectedImage);
+
+                    readExif(photoUri);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case REQUEST_IMAGE_CAPTURE:
+                // by this point we have the camera photo on disk
+                selectedImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ImageView imagePreview = mView.findViewById(R.id.image_preview);
+                imagePreview.setVisibility(View.VISIBLE);
+                imagePreview.setImageBitmap(selectedImage);
+
+                readExif(photoFileUri);
+                break;
+        }
+    }
+
+    /**
+     * Read data from image
+     *
+     * @param uri image uri
+     */
+    void readExif(Uri uri) {
+
+        try {
+            InputStream is = getContext().getContentResolver().openInputStream(uri);
+            ExifInterface exifInterface = new ExifInterface(is);
+
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    imagePreview.setRotation(90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    imagePreview.setRotation(180);
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    imagePreview.setRotation(270);
+                default:
+                    imagePreview.setRotation(0);
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
 
     }
 
+    private static class GrpcProfileImage extends AsyncTask<String, Void, Naturae.ProfileImageReply> {
+        private final OnFragmentInteractionListener mListener;
+        private final WeakReference<Activity> activity;
+        private ManagedChannel channel;
 
-    @Override
-    public void hideKeyboard() {
+        private GrpcProfileImage(OnFragmentInteractionListener mListener, Activity activity) {
+            this.mListener = mListener;
+            this.activity = new WeakReference<>(activity);
+        }
 
-    }
+        @Override
+        protected Naturae.ProfileImageReply doInBackground(String... params) {
+            Naturae.ProfileImageReply reply;
+            try {
+                //Create a channel to connect to the server
+                channel = ManagedChannelBuilder.forAddress(Constants.HOST, Constants.PORT).useTransportSecurity().build();
+                //Create a stub for with the channel
+                ServerRequestsGrpc.ServerRequestsBlockingStub stub = ServerRequestsGrpc.newBlockingStub(channel);
 
-    @Override
-    public void beginFragment(StartUpActivityContainer.AuthFragmentType fragmentType, boolean setTransition, boolean addToBackStack) {
+                //Create an gRPC create account request
+                Naturae.ProfileImageRequest request = Naturae.ProfileImageRequest.newBuilder()
+                        .setAppKey(Constants.NATURAE_APP_KEY)
+                        .setAccessToken(UserUtilities.getAccessToken(activity.get())).build();
+                        //todo
 
-    }
 
-    @Override
-    public void startMainActivity() {
+                //Send the request to the server and set reply to equal the response back from the server
+                reply = stub.getProfileImage(request);
+
+            } catch (Exception e) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                pw.flush();
+                return null;
+            }
+            return reply;
+
+        }
+
+        @Override
+        protected void onPostExecute(Naturae.ProfileImageReply reply) {
+            super.onPostExecute(reply);
+            //Shut down the gRPC channel
+            try {
+                channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            //Check if reply is equal to null. If it's equal to null then there was an error with the server or phone
+            //while communicating with the server.
+            if (reply != null) {
+
+            } else {
+
+            }
+
+        }
+
+        /**
+         * Create an dialog box that display the error
+         *
+         * @param message the error message to be display
+         */
+        private void displayError(String message) {
+            //Create an instance of Alert Dialog
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity.get());
+            alertDialogBuilder.setTitle("Error").setMessage(message).setPositiveButton(R.string.ok, (dialog, which) -> {
+                dialog.cancel();
+            }).show();
+        }
 
     }
 }
